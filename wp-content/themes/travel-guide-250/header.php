4a65893e9eeeb3c52f0de2a5b645d1bef4f0535b
Kara-B <kjbutler819@verizon.net>
2023-02-21 00:39:22 -0500

    :sparkles: Header and Footer Update

---
+++ wp-content/themes/travel-guide-250/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/styles/main.css">
+    <title>Visit The Inner Harbor</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<div class="menuHeader">
+    <h3 class="navItem"> Things to Do </h3>
+    <h3 class="navItem"> History </h3>
+    <h3 class="navItem"> Travel </h3>
+
+</div>
