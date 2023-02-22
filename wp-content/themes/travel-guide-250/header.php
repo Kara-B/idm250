c1ea3a8600802de2678311b0bda03d7cea3b8137
Kara-B <kjbutler819@verizon.net>
2023-02-21 19:04:34 -0500

    Update header.php

--- wp-content/themes/travel-guide-250/header.php
+++ wp-content/themes/travel-guide-250/header.php
@@ -9,9 +9,15 @@
     <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?> >
+<?php
+  // @link https://developer.wordpress.org/reference/functions/wp_body_open/
+  // Fires the wp_body_open action.
+  wp_body_open();
+  ?>
 <div class="menuHeader">
     <h3 class="navItem"> Things to Do </h3>
     <h3 class="navItem"> History </h3>
     <h3 class="navItem"> Travel </h3>
 </div>
+<?php wp_nav_menu(['theme_location' => 'primary-menu']); ?>
