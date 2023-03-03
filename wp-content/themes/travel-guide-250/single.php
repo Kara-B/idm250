1ee77bf513838100aae4c22421e60595c8ccc1bb
Kara-B <kjbutler819@verizon.net>
2023-03-03 15:46:28 -0500

    Update single.php

--- wp-content/themes/travel-guide-250/single.php
+++ wp-content/themes/travel-guide-250/single.php
@@ -1,5 +1,5 @@
 
-<div class="hero">
+<!-- <div class="hero">
     <div>
       <img src="https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80">  
     </div>
@@ -17,6 +17,6 @@
   <div>
     <button> Buy Tickets </button>
     <button> Explore Other Events </button>
-  </div>
+  </div> -->
 
   
