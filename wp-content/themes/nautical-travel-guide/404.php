4e2b6f919a661ae5eee998f2000b6e23ba826876
Kara-B <kjbutler819@verizon.net>
2023-03-06 14:06:48 -0500

    Files Restored

---
+++ wp-content/themes/nautical-travel-guide/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div>
+    <img class="404Number" src="<?php echo get_template_directory_uri(); ?>/dist/images/404.svg" alt="404" >
+    <h1> UH OH! </h1>
+</div>
+
+<p> It looks like you made a wrong turn, there's nothing to see here. </p>
+<button> Back Home </button>
+<!-- <?php get_template_part('components/content'); ?> -->
+<?php get_footer(); ?>
---
+++ wp-content/themes/nautical-travel-guide/components/archive_card.php
@@ -0,0 +1,1 @@
+<!-- this is where we format the card in our archive -->
