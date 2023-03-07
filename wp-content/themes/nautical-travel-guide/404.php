feb24256506fb7aaa294b9c11c1422598ebfbbff
Kara-B <kjbutler819@verizon.net>
2023-03-06 23:28:18 -0500

    Update 404.php

--- wp-content/themes/nautical-travel-guide/404.php
+++ wp-content/themes/nautical-travel-guide/404.php
@@ -3,8 +3,7 @@
     <img class="404Number" src="<?php echo get_template_directory_uri(); ?>/dist/images/404.svg" alt="404" >
     <h1> UH OH! </h1>
 </div>
-
 <p> It looks like you made a wrong turn, there's nothing to see here. </p>
 <button> Back Home </button>
-<!-- <?php get_template_part('components/content'); ?> -->
+
 <?php get_footer(); ?>
