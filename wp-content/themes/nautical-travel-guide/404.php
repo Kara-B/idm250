443eb398876f8d26c235a0bd59c70ebd5251f509
Kara-B <kjbutler819@verizon.net>
2023-03-07 15:13:50 -0500

    404 Update

--- wp-content/themes/nautical-travel-guide/404.php
+++ wp-content/themes/nautical-travel-guide/404.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
-<div>
-    <img class="404Number" src="<?php echo get_template_directory_uri(); ?>/dist/images/404.svg" alt="404" >
+<div class="column404">
+    <div>
+        <img class="flysvg" src="<?php echo get_template_directory_uri(); ?>/dist/images/fly1.svg" alt="a fly flying around" >
+        <img class="404Number" src="<?php echo get_template_directory_uri(); ?>/dist/images/404.svg" alt="404" >
+        <img class="flysvg" src="<?php echo get_template_directory_uri(); ?>/dist/images/fly2.svg" alt="a fly flying around" >
     <h1> UH OH! </h1>
+    </div>
+    <p> It looks like you made a wrong turn, there's nothing to see here. </p>
+    <button> Back Home </button>
 </div>
-<p> It looks like you made a wrong turn, there's nothing to see here. </p>
-<button> Back Home </button>
+
 
 <?php get_footer(); ?>
